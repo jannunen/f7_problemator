74783c352e378357d225a0b0bebf6b05b5df9c37
jannunen <jarmo@annunen.fi>
2022-10-17 13:26:35 +0300

    Updated vite to 3.1.8

--- vite.config.js
+++ vite.config.js
@@ -1,37 +1,47 @@
-import path from 'path';
-import framework7 from 'rollup-plugin-framework7';
+import path from 'path'
+import framework7 from 'rollup-plugin-framework7'
 import vue from '@vitejs/plugin-vue'
+import { defineConfig, loadEnv } from 'vite'
+import basicSsl from '@vitejs/plugin-basic-ssl'
+const fs = require('node:fs');
 
-const SRC_DIR = path.resolve(__dirname, './src');
-const PUBLIC_DIR = path.resolve(__dirname, './public');
-const BUILD_DIR = path.resolve(__dirname, './www');
+const SRC_DIR = path.resolve(__dirname, './src')
+const PUBLIC_DIR = path.resolve(__dirname, './public')
+const BUILD_DIR = path.resolve(__dirname, './www')
 
-export default {
-  plugins: [vue(), framework7({ emitCss: false })],
-  root: SRC_DIR,
-  base: '',
-  publicDir: PUBLIC_DIR,
-  build: {
-    outDir: BUILD_DIR,
-    assetsInlineLimit: 0,
-    emptyOutDir: true,
-  },
-  resolve: {
-    alias: {
-      '@': SRC_DIR,
-      '@js': SRC_DIR+"/js",
-      '@helpers' : SRC_DIR+"/js/helpers",
-      '@auth' : SRC_DIR+"/js/auth",
-      '@components' : SRC_DIR+"/components",
-      '@pages' : SRC_DIR+"/pages"
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    define: {
+      __APP_ENV__: env.APP_ENV
     },
-  },
-  server: {
-    host: true,
-    https : true,
-  },
-  esbuild: {
-    jsxFactory: '$jsx',
-    jsxFragment: '"Fragment"',
-  },
-};
+    plugins: [basicSsl(), vue(), framework7({ emitCss: false })],
+    root: SRC_DIR,
+    base: '',
+    publicDir: PUBLIC_DIR,
+    build: {
+      outDir: BUILD_DIR,
+      assetsInlineLimit: 0,
+      emptyOutDir: true,
+    },
+    resolve: {
+      alias: {
+        '@': SRC_DIR,
+        '@js': SRC_DIR + "/js",
+        '@helpers': SRC_DIR + "/js/helpers",
+        '@auth': SRC_DIR + "/js/auth",
+        '@components': SRC_DIR + "/components",
+        '@pages': SRC_DIR + "/pages"
+      },
+    },
+    server: {
+      host: true,
+      https: true,
+      port : 3000,
+    },
+    esbuild: {
+      jsxFactory: '$jsx',
+      jsxFragment: '"Fragment"',
+    },
+  }
+})
