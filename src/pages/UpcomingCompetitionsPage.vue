4e824ecd62463fbc1d1a7ac67868d4db88e07fde
jannunen <jarmo@annunen.fi>
2022-10-23 15:34:36 +0300

    Changed back links

--- src/pages/UpcomingCompetitionsPage.vue
+++ src/pages/UpcomingCompetitionsPage.vue
@@ -1,7 +1,7 @@
 <template>
   <f7-page >
     <f7-navbar>
-      <f7-nav-left><f7-link back>back</f7-link></f7-nav-left>
+      <f7-nav-left><f7-link href="/home">back</f7-link></f7-nav-left>
       <f7-nav-title> {{ t('comps.upcoming_competitions') }} </f7-nav-title>
     </f7-navbar>
     <f7-block>
