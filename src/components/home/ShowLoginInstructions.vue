c3b7edb6b4de5e65abac2bf300b68dbe7b63481d
jannunen <jarmo@annunen.fi>
2022-10-24 00:22:51 +0300

    Shortened instructions

--- src/components/home/ShowLoginInstructions.vue
+++ src/components/home/ShowLoginInstructions.vue
@@ -7,47 +7,22 @@
         First of all, I am sorry for the long overdue update. There 
         has been a lot of going on. Please read through the following text.
         <br /><br />
-        <h2 class="font-bold my-2">What's new?</h2>
-        In this update, many of the old functionality has been stripped
-        off and they will be rebuilt as we go. The app is rewritten 
-        from scratch with <a href="https://vuejs.org/">Vue.js 3</a>, <a href="https://framework7.io/vue/">Framework7</a> and <a href="https://tailwindcss.com/">Tailwind</a>.
-        New functionality will be added gradually and there will be a popup showing
-        about new features.
-        <br /><br />
-        And there are some new features actually: 
-        <ul class="text-left ml-8 list-disc">
-        <li>Faster!</li>
-        <li>Map based navigation</li>
-        <li>Competition judging integrated</li>
-        <li>Support for QR codes on problem tags (gyms who use this)</li>
-        <li>Ability to have projects</li>
-        <li>Better filtering (grades, walls, projects, climbed problems etc.)</li>
-        </ul>
-        <h2 class="font-bold my-2">Cookies</h2>
+        <h2 class="font-bold my-2">Read this carefully</h2>
+        <div class="p-2 bg-red-600 opacity-80 border border-red-300 rounded-md text-white">
+          <strong class="text-lg"> Do NOT use the old login</strong>
+          Sign up again with the same email you used before
+          OR use Facebook/Google login after clicking login.
+          <br /><br />
+        </div>
+    </p>
+    <p-button class="btn-primary" @click="loginWithRedirect">{{ t('Login / Register') }}</p-button>
+        <h2 class="font-bold my-2">Cookie policy</h2>
         And by the way, we <strong>do use</strong> cookies, as does almost
         every web app. We're not selling your soul to third parties. By
         logging in you accept our usage of cookies. We use cookies to
-        track your climbing, and that's it.
+        track your climbing, and that's it. If you don't want to use 
+        cookies, the app will not work. 
         <br /><br />
-        <h2 class="font-bold my-2">Read this carefully</h2>
-        <div class="p-2 bg-red-600 opacity-80 border border-red-300 rounded-md text-white">
-          <strong class="text-lg">You cannot use your old login/password.</strong> You have to signup or
-          use Facebook/Google login after clicking login.
-          <br /><br />
-          The easiest way is to use the same email address you used to
-          login to the old version OR the same email you have
-          associated to your Facebook login.
-          <br /><br />
-          If you cannot see your old ascents, open the menu (top right) and 
-          choose "Import ticks". Fill in the field "email" and click "Send instructions". Then you will get
-          an email to your old email address and by confirming the email address,
-          you will get your old ticks back!
-
-        </div>
-        
-         Now, please click the login or register button below.
-    </p>
-    <p-button class="btn-primary" @click="loginWithRedirect">{{ t('Login / Register') }}</p-button>
     </div>
     
 </template>
