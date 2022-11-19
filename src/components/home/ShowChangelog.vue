b9201a039755a5016c6aed186fc072044bc87eca
jannunen <jarmo@annunen.fi>
2022-11-19 22:15:49 +0200

    Added info about seeing my own hardest ticks

--- src/components/home/ShowChangelog.vue
+++ src/components/home/ShowChangelog.vue
@@ -16,7 +16,6 @@
           <f7-tab id="tab-1" class="mt-2" tab-active>
             <ul class="list-disc my-2 mx-4 p-2">
               <li class="list-none mt-2 text-green-500">Soon</li>
-              <li>Possibility list your hardest ascents (works from ranking's 'my rank')</li>
               <li>See what other climber's have climbed.</li>
               <li>Jatsin orjat (Slaves of Yahtzee)</li>
               <li>Reporting dirty/dangerous</li>
@@ -27,6 +26,7 @@
             </ul>
           </f7-tab>
           <f7-tab id="tab-2" class="mt-2" >
+            <release-note title="0.9.2" :notes="['Possibility list your hardest ascents (works from ranking\'s \'my rank\')']"></release-note>
             <release-note title="0.8.36 (2022-11-10)" :notes="['Re-added ranking. Let us see if it works']" />
             <release-note title="0.8.33 (2022-10-30)" :notes="['Fixed some issues with small screen','Added change notes']" />
             <release-note title="0.8.32 (2022-10-30)" :notes="['Fixed result view layout','Added better refresh countdown indicator for result list' ]" />
