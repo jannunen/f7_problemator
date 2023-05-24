12bde78432f932fc8efaca3721cc6d20c2da8541
John doe <johndoe@pepedogen.com>
2023-05-24 22:42:31 +0300

    Disabled qr code reader

--- src/components/home/TodayHeader.vue
+++ src/components/home/TodayHeader.vue
@@ -2,7 +2,6 @@
 import ScoreToday from '@components/home/ScoreToday.vue'
 import { useI18n } from 'vue-i18n'
 import { ref } from 'vue'
-import QrSearchSheet from '@components/ui/problem/QrSearchSheet.vue'
 const { t } = useI18n()
 
 const props = defineProps({
@@ -15,17 +14,11 @@ const openQRReader = () => {
 const emit = defineEmits(['addtick'])
 </script>
 <template>
-  <qr-search-sheet @close="qrReaderOpened = false" :opened="qrReaderOpened" @read="onReadQRCode"></qr-search-sheet>
   <div>
     <div class="my-2 text-center text-lg font-bold">{{ t('home.today') }}</div>
     <div class="flex flex-row justify-center mt-3">
       <score-today />
-      <div class="mt-2 mx-1">
-        <f7-link @click="openQRReader" href="#" no-link-class class="ml-2 w-20 h-20 rounded-full bg-purple-800 py-2 px-1 text-white flex flex-col justify-center items-center font-bold">
-          <i class="icon f7-icons">qrcode</i>
-          <small>{{ t('searchprob.read_qr') }}</small>
-        </f7-link>
-      </div>
+      
       <div class="mt-2">
         <button @click="emit('addtick')" class="w-20 h-20 rounded-full bg-purple-800 py-2 px-0 text-white flex flex-col justify-center items-center font-bold">
           <f7-icon material="add" color="white" size="26px"></f7-icon>
