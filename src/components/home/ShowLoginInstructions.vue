0180d572470c0ea302b815b2c7798dce2d7b02bf
John doe <johndoe@pepedogen.com>
2023-06-13 11:50:01 +0300

    Changed the login form

--- src/components/home/ShowLoginInstructions.vue
+++ src/components/home/ShowLoginInstructions.vue
@@ -5,11 +5,15 @@
         
     <p>
         <br /><br />
-        <div class="p-2 bg-red-600  border border-red-300 rounded-md text-white">
-          <div class="font-bold text-2xl text-center">Do the SIGNUP</div>
-          Don't try to login with your old credentials.
-          <br />
+
+        <div class="p-3 bg-sky-600  border border-sky-300 rounded-md text-white w-4/5 mx-auto ">
+          <div class="font-bold text-2xl text-center">Please signup</div>
+              If you have existing credentials, they don't work anymore, but the signup will revive them.
         </div>
+
+      <div class="flex justify-center items-center">
+      <img :src="logo" alt="Problemator logo" class="w-48" />
+      </div>
     </p>
     <p-button class="btn-primary" @click="loginWithRedirect">{{ t('Login / Signup') }}</p-button>
         <h2 class="font-bold my-2">Cookie policy</h2>
@@ -26,9 +30,11 @@
 import PButton from '@components/PButton.vue'
 import { useAuth0 } from '@auth0/auth0-vue'
 import { useI18n } from 'vue-i18n'
+import logo from '../../assets/images/logo.png'
 const {
   loginWithRedirect,
 } = useAuth0()
 const { t } = useI18n()
 
 </script>
+<style></style>
