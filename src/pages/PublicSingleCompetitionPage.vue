0883c6b977374e98b3d0ecca180799600be1b09d
Jarmo Annunen <jarmo@annunen.fi>
2025-01-23 15:32:47 +0100

    Added info if the key had expired

--- src/pages/PublicSingleCompetitionPage.vue
+++ src/pages/PublicSingleCompetitionPage.vue
@@ -5,7 +5,16 @@
       <f7-nav-title> {{ t('comps.standalone_competition_title') }} </f7-nav-title>
     </f7-navbar>
       <f7-block v-if="loading">
-        <div class="text-center mt-2">Loading competition data...</div>
+        <div class="notAuthorized" v-if="notAuthorized">
+          <h3 class="text-center font-bold text-xl text--500 mt-2">Cannot open the competition</h3>
+          <p class="text-center mt-2">
+            <span v-if="notAuthorized">You are not authorized to access this competition. Usually this is because the competition key has expired.
+              <br /><br />
+              Ask the competition organizer to send the registration email again to regain access.
+            </span>
+          </p>
+        </div>
+        <div v-else class="text-center mt-2">Loading competition data...</div>
       </f7-block>
     <f7-block v-if="comp != null">
       <public-competition-entry :comp="comp" />
@@ -28,13 +37,19 @@ const comp = computed(() => store.state.competition)
 const climber = computed(() => store.state.climber)
 const url = window.location.href
 const urlParams = new URLSearchParams(url.split('?')[1])
+const notAuthorized = ref(false)
 // this key can be used to open the entry page without logging in
 const key = urlParams.get('key')
 if (key != null) {
   store.dispatch('getClimberByKey', { compid: props.compid, key: key },'/compkey')
-  .then(() => {
-    store.dispatch('publicGetCompetition',{compid:props.compid, point_entry_key: key})
-    .then(() => loading.value=false)
+  .then((ret) => {
+    debugger
+    if (ret.match(/access.to.this.comp/i)) {
+      notAuthorized.value = true
+    } else {
+      store.dispatch('publicGetCompetition',{compid:props.compid, point_entry_key: key})
+      .then(() => loading.value=false)
+    }
   })
 } else {
       /*
